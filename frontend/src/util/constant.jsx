fe826166ef996a84aeb74137ea38d309f723c590
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2025-01-15 16:36:29 +0530

    upload

--- frontend/src/util/constant.jsx
+++ frontend/src/util/constant.jsx
@@ -1,3 +1,3 @@
 // export const BACKEND_URL = 'http://localhost:3001/api';
 
-export const BACKEND_URL = 'https://food-order-ten-sigma.vercel.app/';
+export const BACKEND_URL = 'https://food-order-ten-sigma.vercel.app/api';
