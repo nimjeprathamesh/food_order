16a95053039486c2323cb2705044774a7de5543d
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2025-01-15 16:25:51 +0530

    change

--- frontend/src/util/constant.jsx
+++ frontend/src/util/constant.jsx
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'http://localhost:3000/api';
+// export const BACKEND_URL = 'http://localhost:3001/api';
+
+export const BACKEND_URL = 'https://food-order-ten-sigma.vercel.app/';
