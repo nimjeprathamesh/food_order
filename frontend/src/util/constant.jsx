f92d75c7643431bb6d0b2ee55fe3baa5b168adee
nimjeprathamesh <nimjeprathamesh1@gmail.com>
2025-01-16 00:01:52 +0530

    update

--- frontend/src/util/constant.jsx
+++ frontend/src/util/constant.jsx
@@ -1,3 +1,1 @@
-// export const BACKEND_URL = 'http://localhost:3001/api';
-
-export const BACKEND_URL = 'https://food-order-backend-njgkeotfq-prathamesh-nimjes-projects.vercel.app/api';
+export const BACKEND_URL = 'https://food-order-backend-nine.vercel.app/api';
